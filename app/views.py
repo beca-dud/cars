5331ebe5a7788a313682176872cec07f213bd229
Rebeca Eduarda <rebecaeduarda6391@gmail.com>
2024-11-14 10:04:39 -0300

    Mais atualizações

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from app.models import Car
 
 # Create your views here.
 def index(request):
     cars = Car.objects.all()
     return render(request, 'index.html', {'cars': cars})
+
+def devs(request):
+    if request.user.is_authenticated:
+        return render(request, 'desenvolvedores.html')
+    
+    return redirect('/admin/login/?next=/')
