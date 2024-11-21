3135e3d683ef7a8a5373ce8259a42fe83658c1bf
Rebeca Eduarda <rebecaeduarda6391@gmail.com>
2024-11-21 07:50:35 -0300

    Últimas alterações

--- app/admin.py
+++ app/admin.py
@@ -4,7 +4,7 @@ from app.models import Car
 # Register your models here.
 
 class CarFilter(admin.ModelAdmin):
-    list_display = ("id", "model", "brand", "year")
+    list_display = ("id", "user", "model", "brand", "year")
     list_display_links = ("id", "model", "brand", "year")
     list_filter = ("brand", "year")
     search_fields = ("model", "brand")
